defd1ce2ee880ca9a2483d69d0d68f67994be25e
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-26 17:22:51 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 			$(this).addClass('open');
 			$('#blt-container').addClass('open');
 			//open menu
+			stackTheSandwich();
 			resolved++;
 		}
 		if(resolved===0&&$(this).hasClass('open')) {
@@ -30,3 +31,42 @@ $(document).ready(function() {
 		console.log(resolved);
 	})
 })
+
+function stackTheSandwich(x) {
+	menu=[
+		{item:'Products',sandwich:'products',link:'none'},
+		{item:'Stores & Markets',sandwich:'s_and_m',link:'none'},
+		{item:'About Us',sandwich:'none',link:'/about-us.html'},
+		{item:'Gallery',sandwich:'none',link:'/gallery.html'}
+	]
+	if(x==null) {
+		x=menu;
+	} else {
+		console.log(x);
+	}
+	//build X
+	bltc=$('#blt-container');
+	loc=location.href;
+	for(i=0;i<x.length;i++) {
+		if(!bltc.children('#'+x[i].item+'Menu')) {
+			bltc.append('<div id="'+x[i].item+'Menu" class="menuItem">'+x[i].item+'</div>');
+			$('#'+x[i].item+'Menu').click(function() {
+				stackTheSandwich(x[i].sandwich);
+			})
+		}
+		if(x[i].link!=='none'&&loc.indexOf(x[i].link)!=-1) {
+			$('#'+x[i].item+'Menu').addClass('active');
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
