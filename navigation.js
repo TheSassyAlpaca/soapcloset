aba7e0c5771f3c6a295aa077d804c0094d37e414
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-26 16:42:28 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,19 +1,21 @@
-$('#blt').click(function() {
-	resolved=0;
-	console.log(resolved);
-	if(resolved===0&&!$(this).hasClass('open')&&!$(this).hasClass('deep')) {
-		$(this).addClass('open');
-		//open menu
-		resolved++;
-	}
-	if(resolved===0&&$(this).hasClass('open')) {
-		$(this).removeClass('open');
-		//menu closes
-		resolved++;
-	}
-	if(resolved===0&&$(this).hasClass('deep')) {
-		$(this).addClass('open').removeClass('deep');
-		resolved++;
-	}
-	console.log(resolved);
+$(document).ready(function() {
+	$('#blt').click(function() {
+		resolved=0;
+		console.log(resolved);
+		if(resolved===0&&!$(this).hasClass('open')&&!$(this).hasClass('deep')) {
+			$(this).addClass('open');
+			//open menu
+			resolved++;
+		}
+		if(resolved===0&&$(this).hasClass('open')) {
+			$(this).removeClass('open');
+			//menu closes
+			resolved++;
+		}
+		if(resolved===0&&$(this).hasClass('deep')) {
+			$(this).addClass('open').removeClass('deep');
+			resolved++;
+		}
+		console.log(resolved);
+	})
 })
