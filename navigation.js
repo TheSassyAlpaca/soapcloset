dc000cfd9e7229261ac736244a77cfd15b0c1b67
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-26 16:47:40 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -4,16 +4,27 @@ $(document).ready(function() {
 		console.log(resolved);
 		if(resolved===0&&!$(this).hasClass('open')&&!$(this).hasClass('deep')) {
 			$(this).addClass('open');
+			$('#blt-container').addClass('open');
 			//open menu
 			resolved++;
 		}
 		if(resolved===0&&$(this).hasClass('open')) {
 			$(this).removeClass('open');
+			$('#blt-container').removeClass('open');
+			$('.blt-submenu').each(function() {
+				$(this).remove();
+			})
 			//menu closes
 			resolved++;
 		}
 		if(resolved===0&&$(this).hasClass('deep')) {
 			$(this).addClass('open').removeClass('deep');
+			if(!$('#blt-container').hasClass('open')) {
+				$('#blt-container').addClass('open');
+			}
+			$('.blt-submenu').each(function() {
+				$(this).remove();
+			})
 			resolved++;
 		}
 		console.log(resolved);
