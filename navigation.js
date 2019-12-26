b5f56933ff99772fe23403f4acab936de17587a9
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-26 16:31:25 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,1 +1,17 @@
-test
+$('#blt').click(funciton() {
+	resolved=0;
+	if(resolved===0&&!$(this).hasClass('open')&&!$(this).hasClass('deep')) {
+		$(this).addClass('open');
+		//open menu
+		resolved++;
+	}
+	if(resolved===0&&$(this).hasClass('open')) {
+		$(this).removeClass('open');
+		//menu closes
+		resolved++;
+	}
+	if(resolved===0&&$(this).hasClass('deep')) {
+		$(this).addClass('open').removeClass('deep');
+		resolved++;
+	}
+})
