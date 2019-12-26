9d1b86b3518f4ba6e6eaa4a4d08b6cf3c085c65b
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-26 15:12:41 -0500

    Create friendface.js

---
+++ friendface.js
@@ -0,0 +1,51 @@
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '584847252330296',
+		cookie     : true,
+		xfbml      : true,
+		version    : 'v5.0'
+	});
+	FB.AppEvents.logPageView();
+	checkLoginState();
+};
+
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "https://connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+		//statusChangeCallback(response);
+		console.log(response);
+		if(response.status==='connected') {
+			console.log(response.authResponse.userID);
+			console.log('https://graph.facebook.com/'+response.authResponse.userID+'/picture?type=square');
+			$('#login-button').css('display','none');
+			$('#logout').css('display','block');
+		} else {
+			console.log("Error?");
+		}
+	});
+}
+
+function endLoginStatus() {
+	FB.logout(function(response) {
+		console.log(response);
+		if(response.status!=='connected') {
+			console.log("Logout responded");
+			$('#logout').css('display','none');
+			$('#login-button').css('display','block');
+		}
+	});
+}
+
+$(function() {
+	$('#logout').click(function() {
+		console.log("Logout clicked");
+		endLoginStatus();
+	});
+})
