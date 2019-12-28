95c3d1be6de03c62a18ef7648f624f49613e03b7
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-28 11:04:55 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,3 +1,15 @@
+//onload set event to Sign In button for loggin in modal
+//onload check if user is logged in and hide Sign In button
+//on sign in hide Sign In Button and show userCart
+
+
 function createOrFindUser(id,type) {
-	
+	//what is there to create?
+	$('#userLog').css('display','none');
+	$('#userCart').css('display','block');
+}
+
+function endUser(type) {
+	$('#userLog').css('display','block');
+	$('#userCart').css('display','none');
 }
