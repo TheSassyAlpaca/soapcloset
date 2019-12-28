a8002fbfd634c71ac120ed411f94e99d8a3620e2
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-27 19:31:26 -0500

    Create loginModal.js

---
+++ loginModal.js
@@ -0,0 +1,16 @@
+modal=`
+<modal id="loginModal">
+	<h1>Sign Up</h1>
+	<span>To shop, review orders, build a monthly re-occurring delivery, store favorites, create a wishlist, review products, and subscribe to updates.</span>
+	<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="true" data-use-continue-as="true"></div>
+	<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="true" data-use-continue-as="true"></div>
+	<div>
+		<a href="/legal/privacy%20policy.html">Privacy Policy</a>
+		<a href="/legal/terms%20of%20service.html">Terms of Service</a>
+	</div>
+	<span>Copyright © 2019-2020 The Sassy Alpaca, LLC. All Rights Reserved</span>
+</modal>`
+
+$(function() {
+	$('body').append('<div id="modalShield">'+modal+'</div>');
+})
