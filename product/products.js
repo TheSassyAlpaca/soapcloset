8891e15d9a3247b632caf986e018610d61ae869b
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-28 15:27:49 -0500

    Update products.js

--- product/products.js
+++ product/products.js
@@ -1,6 +1,57 @@
+$(function() {
+	//are there variables in url???
+	pKey="1qu4IlBEElSjAsX0E6ZetEQxL16BuMdjrb-l3EoU21iU";
+	pName=location.href.substring(location.href.indexOf('/product/')+9,endString(location.href,'?'));
+	console.log(pName);
+	$(function() {
+		$.getJSON("https://spreadsheets.google.com/feeds/list/" + pKey + "/1/public/values?alt=json-in-script&callback=?",
+		function (data) {
+			$.each(data.feed.entry, function(i,entry) {
+				console.log(entry);
+				console.log(entry.gsx$proname.$t);
+				if(entry.gsx$proname.$t===pName) {
+					buildPage(entry);
+				}
+			});
+			//dataPulls("getSongs");
+		});
+	});
+})
+
+function buildPage(x) {
+	console.log(endString(x.gsx$price.$t,','));
+	c=$('#content');
+	c.append('<h1>'+x.gsx$productname.$t+'</h1>');
+	c.append('<span>'+x.gsx$traits.$t+'</span>');
+	c.append(buildSlide(x.gsx$photos.$t));
+	c.append('<h2>$'+x.gsx$price.$t.substring(0,endString(x.gsx$price.$t,','))+' per '+x.gsx$size.$t+'</h2><br>');
+	if(x.gsx$size.$t.indexOf(',')!=-1) {
+		c.append('<h2>Bulk Rates:</h2><br>'+presentBulk(x.gsx$size.$t));
+	}
+	c.append('<div>'+x.gsx$description.$t+'</div><br>');
+	c.append('<div>'+buildIngredients(x.gsx$ingredients.$t)+'</div><br>');
+	
+	
+	//name - h1
+	//traits, listed
+	//images - parse and create no-time slide show
+	//price - size
+	//remaining prices
+	//description
+	//ingredients - parse list of ingredients and create tooltips for each ingredient
+	//THE ABOVE IS DONE
+	
+	
+	//bundles - this gets pulled from elsewhere
+	//frequently bought with - pulled from elsewhere
+	
+	
+	//articles - list of hosted articles related to this product
+	
+}
 
 
-function end(x,y) {
+function endString(x,y) {
 	end=x.length;
 	if(x.indexOf(y)!=-1) {
 		end=x.indexOf(y);
@@ -28,59 +79,6 @@ function presentBulk(x) {
 }
 
 function buildIngredients(x) {
-	
-}
-
-
-$(function() {
-	//are there variables in url???
-	pKey="1qu4IlBEElSjAsX0E6ZetEQxL16BuMdjrb-l3EoU21iU";
-	pName=location.href.substring(location.href.indexOf('/product/')+9,end(location.href,'?'));
-	console.log(pName);
-	$(function() {
-		$.getJSON("https://spreadsheets.google.com/feeds/list/" + pKey + "/1/public/values?alt=json-in-script&callback=?",
-		function (data) {
-			$.each(data.feed.entry, function(i,entry) {
-				console.log(entry);
-				console.log(entry.gsx$proname.$t);
-				if(entry.gsx$proname.$t===pName) {
-					buildPage(entry);
-				}
-			});
-			//dataPulls("getSongs");
-		});
-	});
-})
-
-function buildPage(x) {
-	console.log(end(x.gsx$price.$t,','));
-	c=$('#content');
-	c.append('<h1>'+x.gsx$productname.$t+'</h1>');
-	c.append('<span>'+x.gsx$traits.$t+'</span>');
-	c.append(buildSlide(x.gsx$photos.$t));
-	c.append('<h2>$'+x.gsx$price.$t.substring(0,end(x.gsx$price.$t,','))+' per '+x.gsx$size.$t+'</h2><br>');
-	if(x.gsx$size.$t.indexOf(',')!=-1) {
-		c.append('<h2>Bulk Rates:</h2><br>'+presentBulk(x.gsx$size.$t));
-	}
-	c.append('<div>'+x.gsx$description.$t+'</div><br>');
-	c.append('<div>'+buildIngredients(x.gsx$ingredients.$t)+'</div><br>');
-	
-	
-	//name - h1
-	//traits, listed
-	//images - parse and create no-time slide show
-	//price - size
-	//remaining prices
-	//description
-	//ingredients - parse list of ingredients and create tooltips for each ingredient
-	//THE ABOVE IS DONE
-	
-	
-	//bundles - this gets pulled from elsewhere
-	//frequently bought with - pulled from elsewhere
-	
-	
-	//articles - list of hosted articles related to this product
 	
 }
 
