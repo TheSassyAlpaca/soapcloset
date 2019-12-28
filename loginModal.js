277f00e51a94e1f261c018c8289ae05cc7452c23
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-27 19:54:34 -0500

    Update loginModal.js

--- loginModal.js
+++ loginModal.js
@@ -2,8 +2,8 @@ modal=`
 <modal id="loginModal">
 	<h1>Sign Up</h1>
 	<span>To shop, review orders, build a monthly re-occurring delivery, store favorites, create a wishlist, review products, and subscribe to updates.</span>
-	<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="true" data-use-continue-as="true"></div>
-	<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="true" data-use-continue-as="true"></div>
+	<div class="fb-login-button" data-width="260px" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+	<div class="fb-login-button" data-width="260px" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
 	<div>
 		<a href="/legal/privacy%20policy.html">Privacy Policy</a>
 		<a href="/legal/terms%20of%20service.html">Terms of Service</a>
