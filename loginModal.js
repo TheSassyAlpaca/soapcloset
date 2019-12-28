b3ba8407f4335898486d44721bc80ad858bca4e5
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-27 20:19:08 -0500

    Update loginModal.js

--- loginModal.js
+++ loginModal.js
@@ -2,13 +2,15 @@ modal=`
 <modal id="loginModal">
 	<h1>Sign Up</h1>
 	<span>To shop, review orders, build a monthly re-occurring delivery, store favorites, create a wishlist, review products, and subscribe to updates.</span>
-	<div class="fb-login-button" data-width="260px" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
-	<div class="fb-login-button" data-width="260px" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+	<div class="login-box">
+		<div class="fb-login-button" data-width="260px" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+		<div class="fb-login-button" data-width="260px" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+	</div>
 	<div>
 		<a href="/legal/privacy%20policy.html">Privacy Policy</a>
 		<a href="/legal/terms%20of%20service.html">Terms of Service</a>
 	</div>
-	<span>Copyright © 2019-2020 The Sassy Alpaca, LLC. All Rights Reserved</span>
+	<span class="finePrint">Copyright © 2019-2020 The Sassy Alpaca, LLC. All Rights Reserved</span>
 </modal>`
 
 $(function() {
