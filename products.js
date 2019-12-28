5568a2474773e2de97d22ac0dc3f5200e589249a
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-28 12:26:42 -0500

    Create products.js

---
+++ products.js
@@ -0,0 +1,27 @@
+pKey="2PACX-1vR80HM2qiXn6CLcIpPrIbLFd_4YZ2g9dLRR9Xo54pa8iQG6S7AW-1LjuHvdVGqdzDHBJift-I30Jnzv";
+
+$(function() {
+  getProducts(pKey,1);
+})
+
+function getProduct(x,y) {
+	$(function() {
+		$.getJSON("https://spreadsheets.google.com/feeds/list/" + x + "/" + y + "/public/values?alt=json-in-script&callback=?",
+		function (data) {
+			$.each(data.feed.entry, function(i,entry) {
+				console.log(entry);
+				console.log(entry.gsx$pro_name.$t);
+				//thisEntry=JSON.parse(entry.gsx$data.$t.replace(/'/g,"\""));
+				
+				/*
+				if(checkSongs.indexOf(thisEntry.songID)==-1) {
+					checkSongs.push(thisEntry.songID);
+					window[s].push(thisEntry);
+				} else {
+					window[s][checkSongs.indexOf(thisEntry.songID)]=thisEntry;
+				}*/
+			});
+			//dataPulls("getSongs");
+		});
+	});
+}
