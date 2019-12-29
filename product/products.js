9b4653508afe1f5f68d46865409b3c308074a7f3
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2019-12-29 14:17:21 -0500

    Update products.js

--- product/products.js
+++ product/products.js
@@ -23,14 +23,21 @@ function buildPage(x) {
 	c=$('#content');
 	c.append('<h1>'+x.gsx$productname.$t+'</h1>');
 	c.append('<span>'+x.gsx$traits.$t+'</span>');
-	c.append(buildSlide(x.gsx$photos.$t));
+	c.append('<div id="slideContainer">'+buildSlide(x.gsx$photos.$t)+'</div>');
 	mechanizeSlide(x.gsx$photos.$t);
 	c.append('<div id="priceBox">$'+x.gsx$price.$t.substring(0,endString(x.gsx$price.$t,','))+' <div><span>per </span><span>'+x.gsx$size.$t+'</span></div></div>');
-	if(x.gsx$size.$t.indexOf(',')!=-1) {
-		c.append('<h2>Bulk Rates:</h2><br>'+presentBulk(x.gsx$size.$t));
+	if(x.gsx$price.$t.indexOf(',')!=-1) {
+		c.append('<h2>Bulk Rates:</h2><br>'+presentBulk(x.gsx$price.$t));
 	}
 	c.append('<div>'+x.gsx$description.$t+'</div><br>');
-	c.append('<div>'+buildIngredients(x.gsx$ingredients.$t)+'</div><br>');
+	if(x.gsx$var.$t==='NA') {
+		c.append('<div id="ingredients">'+buildIngredients(x.gsx$ingredients.$t)+'</div><br>');
+	} else {
+		c.append('<div id="ingredients"></div><br>');
+		buildVars(x.gsx$var.$t);
+		$('#ingredients').append();
+	}
+	
 	
 	
 	//name - h1
@@ -60,16 +67,43 @@ function endString(x,y) {
 	return end
 }
 
+function buildVars(x) {
+	x=x.split(',');
+	variables=x;
+	for(i=0;i<x.length;i++) {
+		window[x[i]]=[];
+	}
+	pKey="1qu4IlBEElSjAsX0E6ZetEQxL16BuMdjrb-l3EoU21iU";
+	console.log(variables);
+	$(function() {
+		$.getJSON("https://spreadsheets.google.com/feeds/list/" + pKey + "/2/public/values?alt=json-in-script&callback=?",
+		function (data) {
+			$.each(data.feed.entry, function(i,entry) {
+				console.log(entry);
+				console.log(entry.gsx$var.$t);
+				if(x.indexOf(entry.gsx$var.$t)!==-1) {
+					if(window[entry.gsx$var.$t].length===0) {
+						window[entry.gsx$var.$t].push(entry.gsx$varname.$t);
+					}
+					window[entry.gsx$var.$t].push('{"id"="'+entry.gsx$id.$t+'","name"="'+entry.gsx$name.$t+'","ingredients"="'+entry.gsx$ingredients.$t+'"}');
+				}
+			});
+			console.log(Oils);
+			console.log(window[x[0]]);
+			//dataPulls("getSongs");
+		});
+	});
+}
+
 function buildSlide(x) {
 	iURL='/images/products/';
 	x=x.split(',');
-	container='<div id="slideContainer">';
+	container='';
 	guide='<div id="slideGuide">';
 	for(i=0;i<x.length;i++) {
 		container=container+'<div id="slide'+i+'" class="slide" style="background-image:url('+iURL+x[i]+')"></div>';
 		guide=guide+'<div id="guide'+i+'"  class="thumb" style="background-image:url('+iURL+x[i]+')"></div>';
 	}
-	guide=guide+'</div>';
 	slide=container+guide+'</div>';
 	return slide
 }
@@ -102,9 +136,21 @@ function buildIngredients(x) {
 }
 
 function mechanizeSlide(x) {
-	//x=x.split(',');
+	s=0;
+	g=0;
+	$('.slide').each(function() {
+		if(s===0) {
+			$(this).addClass('active');
+		}
+		s++;
+	}
+	$('.thumb').each(function() {
+		if(g===0) {
+			$(this).addClass('active');
+		}
+		g++;
+	}
 	guide='guide';
-	//slide='slide';
 	$('.thumb').click(function() {
 		$('.thumb').each(function() {
 			$(this).removeClass('active');
