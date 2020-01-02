2db727fc03df3fb48cc2bded69ef5a73c49a26e1
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-01-02 15:48:35 -0500

    Update user.js

--- user.js
+++ user.js
@@ -3,10 +3,14 @@
 //on sign in hide Sign In Button and show userCart
 
 
-function createOrFindUser(id,type) {
+function createOrFindUser(id,auth,type) {
 	//what is there to create?
 	$('#userLog').css('display','none');
 	$('#userCart').css('display','block');
+	if(type=='facebook') {
+		console.log(curl -X GET 'https://graph.facebook.com/'+id+'?fields=first_name,last_name,profile_pic,email&access_token='+auth);
+
+	}
 }
 
 function endUser(type) {
