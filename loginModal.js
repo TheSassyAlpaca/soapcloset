566b9f4bfd38b7fce91fe314c6dcc43d936f4ada
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-01-02 17:53:00 -0500

    Update loginModal.js

--- loginModal.js
+++ loginModal.js
@@ -1,9 +1,7 @@
-modalStart=`<modal id="loginModal">
+modal=`<modal id="loginModal">
 	<h1>Sign Up</h1>
 	<span>To shop, review orders, build a monthly re-occurring delivery, store favorites, create a wishlist, review products, and subscribe to updates.</span>
-	<div class="login-box">`;
-
-modalEnd=`</div>
+	<div class="login-box"></div>
 	<div>
 		<a href="/legal/privacy%20policy.html">Privacy Policy</a>
 		<a href="/legal/terms%20of%20service.html">Terms of Service</a>
@@ -12,19 +10,9 @@ modalEnd=`</div>
 </modal>`;
 
 function openLoginModal() {
-	modalMid='';
 	FB.getLoginStatus(function(response) {
-		if(response.status==='connected') {
-			modalMid=`<div class="fb-login-button" data-width="260px" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true" scope="public_profile,email" onlogin="checkLoginState(),closeModal();"></div>`;
-			modal=modalStart+modalMid+modalEnd;
-			$('body').append('<div id="modalShield">'+modal+'</div>');
-		} else {
-			modalMid='';
-			//login_with
-			modal=modalStart+modalMid+modalEnd;
-			$('body').append('<div id="modalShield">'+modal+'</div>');
-			$('.fb-login-button.fb_iframe_widget.fb_iframe_widget_fluid').appendTo($('.login-box'));
-		}
+		$('body').append('<div id="modalShield">'+modal+'</div>');
+		$('.fb-login-button.fb_iframe_widget.fb_iframe_widget_fluid').appendTo($('.login-box'));
 	});
 	
 	$('.login-box, #modalShield').click(function() {
