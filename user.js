7403d17fd91b6b6ac7ee9846e34940c4665a8830
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-01-03 12:04:21 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,31 +1,45 @@
-//onload set event to Sign In button for loggin in modal
-//onload check if user is logged in and hide Sign In button
-//on sign in hide Sign In Button and show userCart
-
-
 function createOrFindUser(id,auth,type) {
-	//what is there to create?
 	$('#userLog').css('display','none');
 	$('#userCart').css('display','block');
-	/*
-	if(type=='facebook') {
-		console.log(curl -X GET 'https://graph.facebook.com/'+id+'?fields=first_name,last_name,profile_pic,email&access_token='+auth);
-
-	}
-	*/
-	url='https://graph.facebook.com/'+id+'?fields=email,first_name,last_name,picture&access_token='+auth;
+	url='https://graph.facebook.com/'+id+'?fields=email&access_token='+auth;
 	FB.api(url, function(response) {
 		if(response.error){
-			console.error(response);
+			console.error(response.error);
 		} else {
-			console.log(response);//Do some stuff with the data object
+			console.log(response);
 			console.log(response.email);
-			console.log(response.picture.data.url);
+			//function to check if email and id are attached
+			checkSubscriber(id,type,response.email);
 		}
 	});
+	//user account page
+	//url='https://graph.facebook.com/'+id+'?fields=email,first_name,last_name,picture&access_token='+auth;
+	//console.log(response.picture.data.url);
 }
 
 function endUser(type) {
 	$('#userLog').css('display','block');
 	$('#userCart').css('display','none');
+}
+
+function checkSubscriber(id,type,email) {
+	sKey='17tAZ9gxLCcj0A1xHOqim2qYlJHwzXFOl6ziDV2rtdAc';
+	checker=0;
+	$(function() {
+		$.getJSON("https://spreadsheets.google.com/feeds/list/" + sKey + "/1/public/values?alt=json-in-script&callback=?",
+		function (data) {
+			$.each(data.feed.entry, function(i,entry) {
+				if(entry.gsx$id.$t==id&&entry.gsx$email.$t==email) {
+					checker++;
+				}
+			});
+			if(checker===0) {
+				$('#subscribe').prev().children('input').val(email).attr('name',id);
+			}
+			if(checker!==0) {
+				//change subscription field
+			}
+			//console.log(Oils[1].id);
+		});
+	});
 }
