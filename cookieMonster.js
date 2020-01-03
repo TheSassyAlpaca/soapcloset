20cb931988fc49da9089fd80d6b0501285e8042b
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-01-03 18:41:45 -0500

    Update cookieMonster.js

--- cookieMonster.js
+++ cookieMonster.js
@@ -30,24 +30,26 @@ function getCookieValue(c,a,v) {
 	if(value!=null&&value!='') {
 		vList=value.split("|");
 		for(i=0;i<vList.length;i++) {
-			vSet=vList[i].split("+");
-			if(vSet[0]==item) {
-				index=i;
-				if(a=="Replace") {
-					newSet=item+"+"+qty;
+			if(vList[i].length>0) {
+				vSet=vList[i].split("+");
+				if(vSet[0]==item) {
+					index=i;
+					if(a=="Replace") {
+						newSet=item+"+"+qty;
+					}
+					if(a=="Add") {
+						newSet=item+"+"+(Number(vSet[1])+qty);
+						console.log("I am adding "+qty+" to "+vSet[1]+".");
+					}
+					if(a=="Subtract") {
+						newSet=item+"+"+(Number(vSet[1])-qty);
+						console.log("I am subtracting "+qty+" from "+vSet[1]+".");
+					}
+					if(a=="Remove"||a=="Delete") {
+						newSet='';
+					}
+					vList.splice(index,1);
 				}
-				if(a=="Add") {
-					newSet=item+"+"+(Number(vSet[1])+qty);
-					console.log("I am adding "+qty+" to "+vSet[1]+".");
-				}
-				if(a=="Subtract") {
-					newSet=item+"+"+(Number(vSet[1])-qty);
-					console.log("I am subtracting "+qty+" from "+vSet[1]+".");
-				}
-				if(a=="Remove"||a=="Delete") {
-					newSet='';
-				}
-				vList.splice(index,1);
 			}
 		}
 		if(index=='x'&&(value==''||value==null)&&(a=="Add"||a=="Replace")) {
