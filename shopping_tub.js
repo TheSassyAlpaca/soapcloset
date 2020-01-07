db876087c510410c0b6dbd6f094568897b6efe35
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-01-07 14:23:06 -0500

    Update shopping_tub.js

--- shopping_tub.js
+++ shopping_tub.js
@@ -1,5 +1,40 @@
+$(function() {
+	openCart();
+})
+
 function openCart() {
 	//do onload of '/shopping_tub'
+	cList=document.cookie.split("; ");
+	c='product';
+	value='';
+	console.log(cList);
+	for(i=0;i<cList.length;i++) {
+		if(cList[i].substring(0,c.length)==c) {
+			cPair=cList[i].split("=");
+			value=cPair[1];
+			console.log(cPair);
+		}
+	}
+	if(value!=null||value!='undefined') {
+		vList=value.split(",");
+		//counter=0;
+		pList=[];
+		for(i=0;i<vList.length;i++) {
+			vSet=vList[i].split('+');
+			if(vSet[1]>0) {
+				//counter++;
+				pList.push('{product:"'+vSet[0]+'",qty:"'+vSet[1]+'"}');
+			}
+		}
+		if(counter>0) {
+			//Looks like there are some items in the cart, lets do something with that...
+			console.log(pList);
+		} else {
+			//Nothing in your cart. Don't feel bad, we've all been there...
+		}
+	} else {
+		//Nothing in your cart. Don't feel bad, we've all been there...
+	}
 }
 
 function updateCart() {
