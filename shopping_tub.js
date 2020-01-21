bdfdfc2645beeb1bbfed5b68203c5ab9a762f28b
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-01-21 16:53:09 -0500

    Update shopping_tub.js

--- shopping_tub.js
+++ shopping_tub.js
@@ -42,7 +42,7 @@ function openCart() {
 				//,"vars":"'+varsList+'"
 				pList.push(JSON.parse('{"product":"'+vSet[0]+'","qty":"'+vSet[1]+'"}'));
 				checkCase=vSet[0].substring(0,1);
-				if(case==case.toLowerCase()) {
+				if(checkCase==checkCase.toLowerCase()) {
 					pNames.push(vSet[0].substring(0,vSet[0].indexOf("_")));
 				} else {
 					pNames.push(vSet[0]);
