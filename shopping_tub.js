e889f35bc953f919b83cca2d64a3bd053a5c2909
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-01-21 20:25:27 -0500

    Update shopping_tub.js

--- shopping_tub.js
+++ shopping_tub.js
@@ -117,16 +117,16 @@
 						vS=y[i].product.substring(y[i].product.indexOf('_')+1,y[i].product.length);
 						console.log(vS);
 					}
-					$('#cart').append('<div id="'+y[i].product+'" class="product"><img src="https://soapcloset.thesassyalpaca.com/images/products/'+products[j].gsx$photos.$t.substring(0,products[j].gsx$photos.$t.indexOf(','))+'"><div data-price="'+products[j].gsx$price.$t.substring(0,endString(products[j].gsx$price.$t,','))+'" data-content="'+y[i].qty*products[j].gsx$price.$t.substring(0,products[j].gsx$price.$t.indexOf(','))+'"><h3>'+products[j].gsx$productname.$t+'</h3><span>'+vTraits+'</span></div><div id="'+y[i].product+'counter" class="counter"><div class="down">-</div><input type="number" value="'+y[i].qty+'" min="0"><div class="up">+</div></div></div>');
-					thisProduct=y[i].product.replace('/|/'g+,'')+'counter';
+					$('#cart').append('<div id="'+y[i].product.replace(/|/g+,'')+'" class="product"><img src="https://soapcloset.thesassyalpaca.com/images/products/'+products[j].gsx$photos.$t.substring(0,products[j].gsx$photos.$t.indexOf(','))+'"><div data-price="'+products[j].gsx$price.$t.substring(0,endString(products[j].gsx$price.$t,','))+'" data-content="'+y[i].qty*products[j].gsx$price.$t.substring(0,products[j].gsx$price.$t.indexOf(','))+'"><h3>'+products[j].gsx$productname.$t+'</h3><span>'+vTraits+'</span></div><div id="'+y[i].product.replace(/|/g+,'')+'counter" class="counter"><div class="down">-</div><input type="number" value="'+y[i].qty+'" min="0"><div class="up">+</div></div></div>');
+					thisProduct=y[i].product.replace(/|/g+,'')+'counter';
 					$('#'+thisProduct).children('div').click(function() {
 						if($(this).hasClass('down')) {
-							if($('#'+y[i].product+'counter').children('input').val()>0) {
-								$('#'+y[i].product+'counter').children('input').val(Number($('#'+y[i].product+'counter').children('input').val())-1);
+							if($('#'+thisProduct).children('input').val()>0) {
+								$('#'+thisProduct).children('input').val(Number($('#'+thisProduct).children('input').val())-1);
 							}
 						}
 						if($(this).hasClass('up')) {
-							$('#'+y[i].product+'counter').children('input').val(Number($('#'+y[i].product+'counter').children('input').val())+1);
+							$('#'+thisProduct).children('input').val(Number($('#'+thisProduct).children('input').val())+1);
 						}
 					})
 					updateCartItem(y[i].product,y[i].qty);
