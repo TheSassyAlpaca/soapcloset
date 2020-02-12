75347f57708264971f940d888b341e81309de02f
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-12 17:11:10 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,22 @@
-test
+$(function() {
+	downloadProducts();
+})
+
+function downloadProducts() {
+	window['products']=[]
+	$(function() {
+		pKey="1qu4IlBEElSjAsX0E6ZetEQxL16BuMdjrb-l3EoU21iU";
+		$(function() {
+			$.getJSON("https://spreadsheets.google.com/feeds/list/" + pKey + "/4/public/values?alt=json-in-script&callback=?",
+			function (data) {
+				$.each(data.feed.entry, function(i,entry) {
+					p=jQuery.parseJSON(entry.gsx$data.$t);
+					products.push(p);
+					console.log(p.name);
+					console.log(p.name2);
+				});
+				//dataPulls("getSongs");
+			});
+		});
+	})
+}
