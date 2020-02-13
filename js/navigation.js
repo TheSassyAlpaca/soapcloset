fcc96b4ec94de44b4ce65b306862b1b2e332b751
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-12 23:54:11 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -19,11 +19,11 @@ function downloadProducts() {
 						$('#content').append('<div id="'+p.category.replace(/[\s&]/,'')+'" class="category"><h1>'+p.category+'</h1></div>');
 					}
 					console.log($('#'+p.category.replace(/[\s&]/,'')+' #'+p.subcategory.replace(/[\s&]/,'')));
-					if(!$('#'+p.subcategory.replace(/[\s&]/,'')).has($('#'+p.category.replace(/[\s&]/,'')))) {
+					if(!$('#'+p.category.replace(/[\s&]/,'')).has('#'+p.subcategory.replace(/[\s&]/,''))) {
 						console.log($('#'+p.category.replace(/[\s&]/,'')+' #'+p.subcategory.replace(/[\s&]/,'')));
 						$('#'+p.category.replace(/[\s&]/,'')).append('<div id="'+p.subcategory.replace(/[\s&]/,'')+'" class="subcategory"><h2>'+p.subcategory+'</h2></div>');
 					}
-					$('#'+p.category.replace(/[\s&]/,'')+' #'+p.subcategory.replace(/[\s&]/,''))).append(listing(p));
+					$('#'+p.category.replace(/[\s&]/,'')).children($('#'+p.subcategory.replace(/[\s&]/,''))).append(listing(p));
 				});
 				//
 				$('.category').children('h1').click(function() {
