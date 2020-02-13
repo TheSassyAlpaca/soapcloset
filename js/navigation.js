3b3340adabe48993ade98cb17a62d8cbc2c35703
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-13 00:01:20 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -18,12 +18,12 @@ function downloadProducts() {
 					if($('#'+p.category.replace(/[\s&]/,'')).length==0) {
 						$('#content').append('<div id="'+p.category.replace(/[\s&]/,'')+'" class="category"><h1>'+p.category+'</h1></div>');
 					}
-					console.log($('#'+p.category.replace(/[\s&]/,'')).children('#'+p.subcategory.replace(/[\s&]/,'')));
+					//console.log($('#'+p.category.replace(/[\s&]/,'')).children('#'+p.subcategory.replace(/[\s&]/,'')));
 					if($('#'+p.category.replace(/[\s&]/,'')).children('#'+p.subcategory.replace(/[\s&]/,'')).length==0) {
-						console.log($('#'+p.category.replace(/[\s&]/,'')).children('#'+p.subcategory.replace(/[\s&]/,'')));
+						//console.log($('#'+p.category.replace(/[\s&]/,'')).children('#'+p.subcategory.replace(/[\s&]/,'')));
 						$('#'+p.category.replace(/[\s&]/,'')).append('<div id="'+p.subcategory.replace(/[\s&]/,'')+'" class="subcategory"><h2>'+p.subcategory+'</h2></div>');
 					}
-					$('#'+p.category.replace(/[\s&]/,'')).children($('#'+p.subcategory.replace(/[\s&]/,''))).append(listing(p));
+					$('#'+p.category.replace(/[\s&]/,'')).children('#'+p.subcategory.replace(/[\s&]/,'')).append(listing(p));
 				});
 				//
 				$('.category').children('h1').click(function() {
