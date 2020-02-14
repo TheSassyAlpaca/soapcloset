89cac98e93a38d8ac1834cbc21ac15241496a945
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-14 12:57:51 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -84,7 +84,7 @@ function downloadProducts() {
 }
 
 function listing(p) {
-	l='<div id="'+p.name.replace(/[\s&]/,'').toLowerCase()+'" class="listing" data="'+p+'"><div class="listingLeft"><img src="'+p.images[0]+'"></div><div class="listingMid"><h3>'+p.name+'</h3><span class="keywords">'+p.keywords.join(", ")+'</span><br><span class="description">'+p.description+'</span></div><div class="listingRight"><span>$'+p.price+'</span>'+bulkRates(p)+'<div class="buy"><button>Add To Cart</button><div><button class="down">-</button><input type="text" value="'+getValue(p)+'" min="0" max="'+p.qty+'"><button>+</button></div></div></div></div><hr>' 
+	l='<div id="'+p.name.replace(/[\s&]/,'').toLowerCase()+'" class="listing" data="'+p+'"><div class="listingLeft"><img src="'+p.images[0]+'"></div><div class="listingMid"><h3>'+p.name+'</h3><span class="keywords">'+p.keywords.join(", ")+'</span><br><span class="description">'+p.description+'</span></div><div class="listingRight"><span>$'+p.price+'</span>'+bulkRates(p)+'<div class="buy"><button class="addToCart">Add To Cart</button><div><button class="down">&#11206;</button><input type="text" value="'+getValue(p)+'" min="0" max="'+p.qty+'"><button>&#11206;</button></div></div></div></div><hr>' 
 	return l
 }
 
