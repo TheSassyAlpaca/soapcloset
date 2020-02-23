c13209635418a6aa0543cbf4ffd8025a86f1812a
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-23 17:47:31 -0500

    Update products.js

--- products/products.js
+++ products/products.js
@@ -141,6 +141,7 @@ function getValue(p) {
 	name=p.name.replace(/[\s&]/,''.toLowerCase());
 	for(i=0;i<cooks.length;i++) {
 		cookie=cooks[i].split('=');
+		console.log(cookie);
 		if(cookie[0]=='cart') {
 			cart=JSON.parse(cookie[1]);
 			if(name in cart) {
