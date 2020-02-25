c815b56582a07e14391a2eb1d896c1211c9b2639
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-25 01:11:57 -0500

    Update product.js

--- products/product.js
+++ products/product.js
@@ -3,11 +3,8 @@ $(function() {
 })
 
 function getProduct() {
-	p=window.location.href.substring(47,window.location.href.lastIndexOf("/"));
-	console.log(p);
-	console.log(window.location.href);
-	console.log(window.location.href.lastIndexOf("/")+1);
-	console.log(window.location.href.indexOf('#'));
+	getP=window.location.href.substring(47,window.location.href.lastIndexOf("/"));
+	product={};
 	$(function() {
 		pKey="1qu4IlBEElSjAsX0E6ZetEQxL16BuMdjrb-l3EoU21iU";
 		$(function() {
@@ -15,12 +12,59 @@ function getProduct() {
 			function (data) {
 				$.each(data.feed.entry, function(i,entry) {
 					product=JSON.parse(entry.gsx$data.$t);
-					if(product.name.replace(/[\s&'!-#()]/g,'').toLowerCase()==p) {
-						console.log(product);
+					if(product.name.replace(/[\s&'!-#()]/g,'').toLowerCase()==getP) {
+						
 					}
 				});
-				//
+				//title
+				//keywords
+				$('#content').append('<div id="title"><h1>'+product.name+'</h1><span>'+listKeywords(product.keywords)+'</span></div>');
+				//image slides
+					//image thumbs
+					//slide mechanics
+				$('#content').append('<div id="slideshow"><div id="slideHolder"></div></div>');
+				$('#slideshow').append('<div id="slideThumb">'+slides(product.images)+'</div>');
+				//price
+					//bulk rates
+				$('#content').append('<div id="pricing"><div class="priceBox"><div>$'+product.price+'</div><div><div>'+product.unit+'</div><div class="bulk">'+b.join(', ')+'</div></div></div></div>');
+				//description
+				$('#content').append('<div id="description">'+product.description+'</div>');
+				//ingredients
+				$('#content').append('<div id="ingredients">Ingredients'+ingredients(product.description)+'</div>');
+				//functions
+				//slideshow
+				$('.slide').click(function() {
+					$('#slideHolder').css('background-image',$(this).css('background-image'));
+				})
+				//ingredients tooltip
+				$('.ingredients').on('hover click',function() {
+					console.log($(this).attr('data-source'));
+				})
 			})
 		});
 	});
+}
+
+function ingredients(ing) {
+	ingredients='';
+	for(i=0;i<ing.length;i++) {
+		ingredients=ingredients+'<div class="ingredient" data-source="'+ing[i].replace(/[\s&'!-#()]/g,'').toLowerCase()+'">'+ing[i]+'</div>';
+		if(i<ing.length-1) {
+			ingredients=ingredients+', ';
+		}
+	}
+	return ingredients
+}
+
+function slides(s) {
+	slides='';
+	for(i=0;i<s.length;i++) {
+		slides=slides+'<div class="slide" style="background-image:url(https://soapcloset.thesassyalpaca.com'+s[i]+')"></div>';
+	}
+	return slides
+}
+
+function listKeywords(k) {
+	keywords=k.join(', ');
+	return keywords
 }
