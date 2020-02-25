1bae5b2d287d9e0c274726ebdcdd7b27f26c4702
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-24 21:44:54 -0500

    Update product.js

--- products/product.js
+++ products/product.js
@@ -1,1 +1,23 @@
-js
+$(function() {
+	getProduct();
+})
+
+function getProduct() {
+	p=window.location.href.substring(47,window.location.href.indexOf('#'));
+	console.log(p);
+	$(function() {
+		pKey="1qu4IlBEElSjAsX0E6ZetEQxL16BuMdjrb-l3EoU21iU";
+		$(function() {
+			$.getJSON("https://spreadsheets.google.com/feeds/list/" + pKey + "/4/public/values?alt=json-in-script&callback=?",
+			function (data) {
+				$.each(data.feed.entry, function(i,entry) {
+					product=JSON.parse(entry.gsx$data.$t);
+					if(product.name.replace(/[\s&'!-#()]/g,'').toLowerCase()==p) {
+						console.log(product);
+					}
+				});
+				//
+			})
+		});
+	});
+}
