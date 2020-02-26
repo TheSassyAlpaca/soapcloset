e97514e9d9119b3c52ca9206701945a60e9e98a9
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-02-25 19:26:04 -0500

    Update products.js

--- products/products.js
+++ products/products.js
@@ -168,5 +168,5 @@ function changeCookie(c,p,a) {
 		}
 	}
 	cart[name]=a;
-	document.cookie='cart='+JSON.stringify(cart)+'; path: /';
+	document.cookie='cart='+JSON.stringify(cart)+';path: /;domain=.thesassyalpaca.com';
 }
