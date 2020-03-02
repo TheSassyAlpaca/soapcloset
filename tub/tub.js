96d8633f36063f9c602bc977c6abda7daa164eb8
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-03-02 16:50:18 -0500

    Update tub.js

--- tub/tub.js
+++ tub/tub.js
@@ -103,7 +103,7 @@ function downloadProducts() {
 					}
 				})
 				$('#coupon .content').append('<input type="text" placeholder="Enter Coupon Code"><span></span>');
-				$('#coupon input').input(function() {
+				$('#coupon input').on('change keyup',function() {
 					coupon=[
 						{code:'villagemarketplace',disc:'10',text:'This coupon gives you 10% off your entire purcahse and supports Village Marketplace.'},
 						{code:'marketplaceatpaynemill',disc:'10',text:'This coupon gives you 10% off your entire purcahse and supports The Marketplace at Payne Mill.'}
