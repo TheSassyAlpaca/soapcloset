faa0ec8bc49b8744d0ee1521973f8552134bd62d
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-03-15 14:15:12 -0400

    Update sam.js

--- shops_and_markets/sam.js
+++ shops_and_markets/sam.js
@@ -36,7 +36,7 @@ function getShopsAndMarkets() {
 					e=JSON.parse(entry.gsx$data.$t);
 					if(e.type=="shop") {
 						shops.push(e);
-						$('#shops').append('<div id="'+e.name.replace(/[\s&'!-#()]/g,'').toLowerCase()+'" class="event" data-source="'+e.gps+'"><div class="eImg"></div><div eMid"><h2>'+e.name+'</h2><div class="eBody">'+e.frequency+'<br>'+e.address+'<br>'+e.website+'<div class="eSocial">'+e.facebook+e.instagram+'</div><div class="eRight"></div></div>');
+						$('#shops').append('<div id="'+e.name.replace(/[\s&'!-#()]/g,'').toLowerCase()+'" class="event" data-source="'+e.gps+'"><div class="eImg" style="background-image: url('+e.image+')"></div><div class="eMid"><h2>'+e.name+'</h2><div class="eBody">'+e.frequency+'<br>'+e.address.replace(', ','</br>')+'<br>'+e.website+'<div class="eSocial">'+e.facebook+e.instagram+'</div><div class="eRight"></div></div>');
 					} else {
 						markets.push(e);
 					}
