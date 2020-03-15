77fc6491951146251b5f4000ae99c1c2f055ce8a
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-03-15 12:22:34 -0400

    Create sam.js

---
+++ shops_and_markets/sam.js
@@ -0,0 +1,23 @@
+$(function() {
+	//get list of shops and markets from DB
+	//separate shops from markets
+	shops=[];
+	markets=[];
+	//loop through shops and markets respectively and perform their function
+	/*shops will show:
+		image(s)
+		name
+		address
+		website
+		social media (limit - FB,Insta)
+	*/
+	/*markets will show:
+		image(s)
+		name
+		address
+		website
+		frequency (text)
+		social media (limit - FB,Insta)
+	*/
+	
+})
