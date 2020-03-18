784cbc423fdb4dd0fc1ba3c492d5dec5e693e764
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-03-18 14:47:13 -0400

    Update tub.js

--- tub/tub.js
+++ tub/tub.js
@@ -193,10 +193,10 @@ function checkCompletion() {
 			email++;
 		}
 	}
-	if(fulfilled==0||email==0) {
+	if(fulfilled==0||email==0||order.estimate==0) {
 		$('#placeOrder').css('display','none');
 	}
-	if(fulfilled!=0&&email!=0) {
+	if(fulfilled!=0&&email!=0&&order.estimate>=0) {
 		$('#placeOrder').css('display','block');
 	}
 }
