b2593658853f6e40f937278aacf530b27a601a19
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-03-25 11:44:45 -0400

    Update sam.js

--- shops_and_markets/sam.js
+++ shops_and_markets/sam.js
@@ -1,4 +1,4 @@
-map="https://www.google.com/maps/d/embed?mid=1Ur_3A9sCrxrMDNeVaCBaHhCWKslitKgN&ll="
+map="https://www.google.com/maps/d/embed?mid=1Ur_3A9sCrxrMDNeVaCBaHhCWKslitKgN&ll=";
 
 $(function() {
 	$('#content').append('<div id="map"><iframe src="'+map+'32.805007368420775%2C-83.62111061384797&z=11"></iframe></div><div id="samContainer"><div id="shops" class="category"><h1>Shops</h1></div><div id="markets" class="category"><h1>Markets</h1></div></div>');
@@ -18,12 +18,12 @@ function getShopsAndMarkets() {
 					if(e.type=="shop") {
 						shopList.push(e);
 						if(e.address!="NA"&&e.image!="NA") {
-							$('#shops').append('<div id="'+e.name.replace(/[\s&'!-#()]/g,'').toLowerCase()+'" class="event" data-source="'+e.gps+'"><div class="eImg" style="background-image: url('+e.image+')"></div><div class="eMid"><h2>'+e.name+'</h2><div class="eBody">'+e.address.replace(', ','</br>')+'<br><a href="'+e.website+'">Website</a><div class="eSocial"><a href="'+e.facebook+'"><img src="/images/facebook.png"></a><a href="'+e.instagram+'"><img src="/images/instagram.png"></a></div><div class="eRight"></div></div>');
+							$('#shops').append('<div id="'+e.name.replace(/[\s&'!-#()]/g,'').toLowerCase()+'" class="event" data-source="'+e.gps+'"><div class="eImg" style="background-image: url('+e.image+')"></div><div class="eMid"><h2>'+e.name+'</h2><div class="eBody">'+e.address.replace(', ','</br>')+'<br><a href="'+e.website+'" target="_blank">Website</a><div class="eSocial"><a href="'+e.facebook+'" target="_blank"><img src="/images/facebook.png"></a><a href="'+e.instagram+'" target="_blank"><img src="/images/instagram.png"></a></div><div class="eRight"></div></div>');
 						}
 					} else {
 						marketList.push(e);
 						if(e.address!="NA"&&e.image!="NA") {
-							$('#markets').append('<div id="'+e.name.replace(/[\s&'!-#()]/g,'').toLowerCase()+'" class="event" data-source="'+e.gps+'"><div class="eImg" style="background-image: url('+e.image+')"></div><div class="eMid"><h2>'+e.name+'</h2><div class="eBody">'+e.frequency+'<br>'+e.address.replace(', ','</br>')+'<br><a href="'+e.website+'">Website</a><div class="eSocial"><a href="'+e.facebook+'"><img src="/images/facebook.png"></a><a href="'+e.instagram+'"><img src="/images/instagram.png"></a></div><div class="eRight"></div></div>');
+							$('#markets').append('<div id="'+e.name.replace(/[\s&'!-#()]/g,'').toLowerCase()+'" class="event" data-source="'+e.gps+'"><div class="eImg" style="background-image: url('+e.image+')"></div><div class="eMid"><h2>'+e.name+'</h2><div class="eBody">'+e.frequency+'<br>'+e.address.replace(', ','</br>')+'<br><a href="'+e.website+'" target="_blank">Website</a><div class="eSocial"><a href="'+e.facebook+'" target="_blank"><img src="/images/facebook.png"></a><a href="'+e.instagram+'" target="_blank"><img src="/images/instagram.png"></a></div><div class="eRight"></div></div>');
 						}
 					}
 					if(i==0) {
