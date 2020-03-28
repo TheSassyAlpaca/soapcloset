af62bd452c517b9cc8993be09c1eae8ec32677a9
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-03-28 17:10:05 -0400

    Update product.js

--- products/product.js
+++ products/product.js
@@ -90,15 +90,15 @@ function getProduct() {
 				$('#shareTool').click(function() {
 					console.log('clicky');
 					socialMediaShares=[
-						{icon:'facebook.png',href:'https://www.facebook.com/sharer/sharer.php?u='+$(location).attr("href")},
-						{icon:'twitter.png',href:'https://twitter.com/intent/tweet?text='+$(location).attr("href")},
-						{icon:'linkedin.png',href:'https://www.linkedin.com/shareArticle?mini=true&url='+$(location).attr("href")+'&title='+$("meta[property='og:title']").attr("content")+'&summary='+$("meta[property='og:description']").attr("content")+'&source=The Sassy Alpaca'},
-						{icon:'pinterest.png',href:'https://pinterest.com/pin/create/button/?url='+$(location).attr("href")+'&media='+$("meta[property='og:image']").attr("content")+'&description='+$("meta[property='og:description']").attr("content")}
+						{icon:'facebook.png',href:'https://www.facebook.com/sharer/sharer.php?u='+encodeURI($(location).attr("href"))},
+						{icon:'twitter.png',href:'https://twitter.com/intent/tweet?text='+encodeURI($(location).attr("href"))},
+						{icon:'linkedin.png',href:'https://www.linkedin.com/shareArticle?mini=true&url='+encodeURI($(location).attr("href"))+'&title='+encodeURI($("meta[property='og:title']").attr("content"))+'&summary='+encodeURI($("meta[property='og:description']").attr("content"))+'&source=The Sassy Alpaca'},
+						{icon:'pinterest.png',href:'https://pinterest.com/pin/create/button/?url='+encodeURI($(location).attr("href"))+'&media='+encodeURI($("meta[property='og:image']").attr("content"))+'&description='+encodeURI($("meta[property='og:description']").attr("content"))}
 					];
 					console.log(socialMediaShares);
 					$('body').append('<div id="shareModul"><div id="shareContainer"></div></div>');
 					for(s=0;s<socialMediaShares.length;s++) {
-						$('#shareContainer').append('<a href="'+socialMediaShares.href+'" target="_blank"><div style="background-image: url(/images/'+socialMediaShares.icon+')"></div></a>');
+						$('#shareContainer').append('<a href="'+socialMediaShares[s].href+'" target="_blank"><div style="background-image: url(/images/'+socialMediaShares[s].icon+')"></div></a>');
 					}
 					$('#shareModul').click(function() {
 						$('#shareModul').remove();
