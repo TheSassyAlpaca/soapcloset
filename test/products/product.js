2deb0f4e1fad92799359efbbaa2a13be7e8b3910
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-03-29 20:07:00 -0400

    Update product.js

--- test/products/product.js
+++ test/products/product.js
@@ -131,6 +131,11 @@ function getProduct() {
 
 function options(o) {
 	console.log(o);
+	opts='';
+	for(i=0;i<o.length;i++) {
+		opts=opts+'<div id="'+o[i].name.replace(/[\s&'!-#()]/g,'').toLowerCase()+'" class="option"><label>'+o.name+'</label><div class="buy"><button>Add To Cart</button><div><button class="down">D</button><input type="text" value="0" min="0" max="'+o[i].qty+'"><button>U</button></div></div></div>';
+	}
+	return opts
 }
 
 function checkInventory(i,n) {
