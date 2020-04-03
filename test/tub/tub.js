3d9b83e0f6859af8a61a158de2319a99082efcd9
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-04-03 14:24:53 -0400

    Update tub.js

--- test/tub/tub.js
+++ test/tub/tub.js
@@ -78,6 +78,8 @@ function downloadProducts() {
 					}
 					if(i.val()!=0) {
 						$(this).parents('.buy').find('.addToCart').css('display','none');
+						$(this).parents('.listing').find('.listingRight span').text('$'+(i.val()*p.price));
+						$(this).parents('.listing').find('.listingRight').attr('data-source',i.val()*p.price);
 					} else {
 						$(this).parents('.buy').find('.addToCart').css('display','block');
 					}
