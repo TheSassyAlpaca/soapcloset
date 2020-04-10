fab27251778acd481c988d3352429aafd1935c23
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-04-10 14:59:37 -0400

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -1,3 +1,22 @@
 $(function() {
 	$('#content').addClass('blogIt');
+	$('#content.blogIt .blogBanner[0]').append('<div id="shareTool"></div><div id="slideHolder" style="background-image:url('+p.images[0]+')"></div>');
+	$('#shareTool').click(function() {
+		console.log('clicky');
+		//test and update the objects as needed...
+		socialMediaShares=[
+			{icon:'facebook.png',href:'https://www.facebook.com/sharer/sharer.php?u='+encodeURI($(location).attr("href"))},
+			{icon:'twitter.png',href:'https://twitter.com/intent/tweet?text='+encodeURI($(location).attr("href"))},
+			{icon:'linkedin.png',href:'https://www.linkedin.com/shareArticle?mini=true&url='+encodeURI($(location).attr("href"))+'&title='+encodeURI(p.name)+'&summary='+encodeURI(p.description)+'&source=The Sassy Alpaca'},
+			{icon:'pinterest.png',href:'https://pinterest.com/pin/create/button/?url='+encodeURI($(location).attr("href"))+'&media='+encodeURI($("meta[property='og:image']").attr("content"))+'&description='+encodeURI($("meta[property='og:description']").attr("content"))}
+		];
+		console.log(socialMediaShares);
+		$('body').append('<div id="shareModul"><div id="shareContainer"></div></div>');
+		for(s=0;s<socialMediaShares.length;s++) {
+			$('#shareContainer').append('<a href="'+socialMediaShares[s].href+'" target="_blank"><div style="background-image: url(/images/'+socialMediaShares[s].icon+')"></div></a>');
+		}
+		$('#shareModul').click(function() {
+			$('#shareModul').remove();
+		})
+	})
 })
