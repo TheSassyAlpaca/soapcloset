fc8972b8b45dfba2fee88b1341091693318a1f8c
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-04-15 14:20:19 -0400

    Update tub.js

--- tub/tub.js
+++ tub/tub.js
@@ -96,7 +96,7 @@ function downloadProducts() {
 				$('.listingLeft, .listingMid h3').click(function(e) {
 					e.stopPropagation();
 					p=products[$(this).parents('.listing').attr('data')];
-					window.location.href="/products/"+p.name.replace(/[\s&'!-#()]/g,'').toLowerCase();
+					window.location.href="/products/"+p.category.replace(/[\s&'!-#()]/g,'').toLowerCase()+"/"+p.subcategory[0].replace(/[\s&'!-#()]/g,'').toLowerCase()+"/"+p.name.replace(/[\s&'!-#()]/g,'').toLowerCase()+"/";
 				})
 				$('.buy input').each(function() {
 					if($(this).val()!=0) {
