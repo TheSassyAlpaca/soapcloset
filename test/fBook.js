76b98d4512b3345e78f6ecf3bcc6cb3e7d64a321
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-06-09 16:18:53 -0400

    Update fBook.js

--- test/fBook.js
+++ test/fBook.js
@@ -1,36 +1,32 @@
+fBJunk=`<script>
+		  window.fbAsyncInit = function() {
+			FB.init({
+			  appId      : '584847252330296',
+			  cookie     : true,
+			  xfbml      : true,
+			  version    : 'v7.0'
+			});
+			  
+			FB.AppEvents.logPageView();   
+			FB.getLoginStatus(function(response) {
+				console.log(response);
+			});
+		  };
+
+		  (function(d, s, id){
+			 var js, fjs = d.getElementsByTagName(s)[0];
+			 if (d.getElementById(id)) {return;}
+			 js = d.createElement(s); js.id = id;
+			 js.src = "https://connect.facebook.net/en_US/sdk.js";
+			 fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+		</script>`
+fBL=`<div class="fb-login-button" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>`;
 
 $(function() {
 	console.log("fBook loaded!");
+	$('#content').prepend(fBL);
+	$('body').prepend(fBJunk);
 })
 
-/*
-//fLogButton='<div class="fb-login-button" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>';
-/*fBScript=`<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '584847252330296',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v7.0'
-    });
-  };
-</script>
-<script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>`
 
-	
-	
-	
-	
-	
-	
-	
-	
-		<div id="fb-root"></div>
-		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v7.0&appId=584847252330296&autoLogAppEvents=1">
-		FB.getLoginStatus(function(response) {
-		console.log(response);
-		//statusChangeCallback(response);
-	});
-		</script>
-		
-		*/
