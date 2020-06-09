1006f00717915c27a1f044306cb63e53ca0d92f7
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-06-09 16:00:11 -0400

    Update fBook.js

--- test/fBook.js
+++ test/fBook.js
@@ -1,3 +1,9 @@
+
+$(function() {
+	console.log("fBook loaded!");
+})
+
+/*
 //fLogButton='<div class="fb-login-button" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>';
 /*fBScript=`<script>
   window.fbAsyncInit = function() {
@@ -10,14 +16,21 @@
   };
 </script>
 <script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>`
-*/
-$(function() {
-	console.log("fBook loaded!");
-	//$('body').prepend(fBScript);
-	//$('#content').append(fLogButton);
-	FB.getLoginStatus(function(response) {
+
+	
+	
+	
+	
+	
+	
+	
+	
+		<div id="fb-root"></div>
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v7.0&appId=584847252330296&autoLogAppEvents=1">
+		FB.getLoginStatus(function(response) {
 		console.log(response);
 		//statusChangeCallback(response);
 	});
-	
-})
+		</script>
+		
+		*/
