7161bc05f8cbc8c6715290d596516f6859c8b057
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-06-09 15:36:17 -0400

    Update fBook.js

--- test/fBook.js
+++ test/fBook.js
@@ -1,7 +1,19 @@
 fLogButton='<div class="fb-login-button" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>';
+fBScript=`<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : 'your-app-id',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v7.0'
+    });
+  };
+</script>
+<script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>`
 
 $(function() {
 	console.log("fBook loaded!");
+	$('body').prepend(fBScript);
 	$('#content').append(fLogButton);
 	FB.getLoginStatus(function(response) {
 		console.log(response);
