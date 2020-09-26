e3609d762a10accbeed40f7fed01dcb2e97d53d3
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-09-26 11:23:58 -0400

    Update fBook.js

--- test/fBook.js
+++ test/fBook.js
@@ -9,10 +9,15 @@ function buildFacebook() {
 		
 	}
 	#userBox img {
-		
+		vertical-align: top;
+		border-radius: 50%;
+		border: 5px solid rgba(var(--ocean),.6);
+		box-shadow: 0px 0px 10px 0px rgba(var(--oats),.3);
 	}
 	#userBox span {
-		
+		vertical-align: top;
+		line-height: 25px;
+		display: inline-block;
 	}
 	</style>`
 	$('body').append(thisCSS);
