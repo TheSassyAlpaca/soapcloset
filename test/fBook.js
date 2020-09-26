7f9939d7515bd1f9e48159f082ba53d61984774e
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-09-26 11:05:40 -0400

    Update fBook.js

--- test/fBook.js
+++ test/fBook.js
@@ -1,4 +1,19 @@
 function buildFacebook() {
+	$('#bltBody').children('.bltMenu').prepend('<div id="userBox"><img src=""><span></span></div>');
 	$('#bltBody').children('.bltMenu').append('<button style="display:none;" onclick="logIntoFacebook();">Log In?</button>');
 	$('#bltBody').children('.bltMenu').append('<button style="display:none;" onclick="logOutOfFacebook();">Log Out?</button>');
+
+	//add to css
+	thisCSS=`<style>
+	#userBox {
+		
+	}
+	#userBox img {
+		
+	}
+	#userBox span {
+		
+	}
+	</style>`
+	$('body').append(thisCSS);
 }
