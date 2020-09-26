5cd21cf4da778e9080a821d6425931ce54ef5262
TheSassyAlpaca <59174809+TheSassyAlpaca@users.noreply.github.com>
2020-09-26 14:23:07 -0400

    Update fBook.js

--- test/fBook.js
+++ test/fBook.js
@@ -31,9 +31,9 @@ function checkCart(id) {
 		cooks[i]=cSplit[0]+'":"'+cSplit.splice(1);
 	}
 	cooked=cooks.join('","');
-	cooked.replace(/"{|/g,'{');
+	cooked.replace(/"\{|/g,'{');
 	console.log(cooked);
-	cookies=JSON.parse('\{"'+cooked+'"}');
+	cookies=JSON.parse('{"'+cooked+'"}');
 	console.log(cookies);//check
 	console.log(cookies.cart);//check
 	console.log(cookies['cart'+id]);//check - undefined
